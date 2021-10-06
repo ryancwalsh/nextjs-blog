29e5d31708fe8003ee235bb47bb9f01c69c590a8
Ryan Walsh <rcwalsh@gmail.com>
2021-10-06 15:05:44 -0400

    tutorial first steps

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+// https://nextjs.org/learn/basics/assets-metadata-css/styling-tips
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    './pages/**/*.js',
+    './components/**/*.js'
+  ]
+  // ...
+}
