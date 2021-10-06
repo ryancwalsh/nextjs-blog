1e268c6c433c6d95f51f347b127622652e32d847
Ryan Walsh <rcwalsh@gmail.com>
2021-10-06 15:48:17 -0400

    dynamic window title and date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Layout from '../../components/layout'
-
+import Head from 'next/head'
+import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
@@ -22,11 +23,14 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
